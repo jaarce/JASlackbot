5de6ffbceb112b7c44b1248d61a01ed02460cc51
JA Arce <ja@directworksmedia.com>
2016-02-02 02:30:04 +0800

    Added update for models

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Employee(models.Model):
     user = models.OneToOneField(User)
     salarium_password = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.user.username
