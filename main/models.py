34aad8ebee614e0e3e8bfb5d1099561078993bae
JA Arce <ja@directworksmedia.com>
2016-02-02 02:12:08 +0800

    Added attendance model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    user = models.OneToOneField(User)
+    salarium_password = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.user.username
