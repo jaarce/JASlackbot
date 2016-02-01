3d482c4a2085ec45ee6960d9c68c5ee3648bedfe
JA Arce <ja@directworksmedia.com>
2016-02-02 01:53:23 +0800

    Addition of main app

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.http.response import HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import View
+
+
+class SlackBotView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(SlackBotView, self).dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return JsonResponse({'text': 'African or European?'})
