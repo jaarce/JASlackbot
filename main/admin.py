766d9b4382f4923ab564a3a232831fb38761776f
JA Arce <ja@directworksmedia.com>
2016-02-02 02:16:17 +0800

    Admin done

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Employee
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Employee, EmployeeAdmin)
