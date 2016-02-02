1b0dbef9e38623cd5f86acd663d172a77543d612
JA Arce <ja@directworksmedia.com>
2016-02-02 22:23:32 +0800

    Fix for the api_key

--- main/views.py
+++ main/views.py
@@ -69,25 +69,36 @@ class JABotsView(View):
         data = request.POST
         keyword = ' '.join(data['text'].split(' ')[0:])
         message = {'text': 'Hi!'}
-        page = random.randint(0, 200)
+
         if keyword == 'pug me' or 'pug' in keyword:
-            response = requests.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=515e81eaff9cb3f524806bc77b503ba3&text=dog%20pugs&format=json&page=' + str(page) + '&nojsoncallback=1')
+            page = random.randint(0, 200)
+            response = requests.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4952b7932b05d0c12f6ee23b6b47669a&text=dog%20pugs&format=json&page=' + str(page) + '&nojsoncallback=1')
             response = json.loads(response.content)
             # random from 1 to 100
             index = random.randint(0, 50)
-            owner = response['photos']['photo'][index]['owner']
-            image_id = response['photos']['photo'][index]['id']
-            farm = response['photos']['photo'][index]['farm']
-            secret = response['photos']['photo'][index]['secret']
-            server = response['photos']['photo'][index]['server']
-            message = {
-                'fallback': 'Cute pugs everywhere',
-                'title': 'Pugs everywhere!',
-                'text': 'Here\'s your pug!',
-                'image_url': 'https://c2.staticflickr.com/%d/%s/%s_%s.jpg' % (farm, server, image_id, secret)
-            }
+            try:
+                owner = response['photos']['photo'][index]['owner']
+                image_id = response['photos']['photo'][index]['id']
+                farm = response['photos']['photo'][index]['farm']
+                secret = response['photos']['photo'][index]['secret']
+                server = response['photos']['photo'][index]['server']
+                message = {
+                    'fallback': 'Cute pugs everywhere',
+                    'title': 'Pugs everywhere!',
+                    'text': 'Here\'s your pug!',
+                    'image_url': 'https://c2.staticflickr.com/%d/%s/%s_%s.jpg' % (farm, server, image_id, secret)
+                }
+            except:
+                message = {
+                    'fallback': 'Cute pugs everywhere',
+                    'title': 'Pugs everywhere!',
+                    'text': 'Here\'s your pug!',
+                    'image_url': 'https://c2.staticflickr.com/2/1681/24299810009_16fbc3a15c.jpg'
+                }
+
         return JsonResponse({
             'response_type': 'in_channel',
             'text': 'JA says',
             'attachments': [message]
         })
+
