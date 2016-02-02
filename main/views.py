db80c6a985d8833562527739a684a1e95fea2be2
JA Arce <ja@directworksmedia.com>
2016-02-02 22:53:07 +0800

    Added food me

--- main/views.py
+++ main/views.py
@@ -95,6 +95,31 @@ class JABotsView(View):
                     'text': 'Here\'s your pug!',
                     'image_url': 'https://c2.staticflickr.com/2/1681/24299810009_16fbc3a15c.jpg'
                 }
+        elif keyword == 'food me' or 'food' in keyword:
+            page = random.randint(0, 200)
+            response = requests.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4952b7932b05d0c12f6ee23b6b47669a&text=food&format=json&page=' + str(page) + '&nojsoncallback=1')
+            response = json.loads(response.content)
+            # random from 1 to 100
+            index = random.randint(0, 50)
+            try:
+                owner = response['photos']['photo'][index]['owner']
+                image_id = response['photos']['photo'][index]['id']
+                farm = response['photos']['photo'][index]['farm']
+                secret = response['photos']['photo'][index]['secret']
+                server = response['photos']['photo'][index]['server']
+                message = {
+                    'fallback': 'Foods everywhere',
+                    'title': 'Foods everywhere!',
+                    'text': 'Here\'s your food!',
+                    'image_url': 'https://c2.staticflickr.com/%d/%s/%s_%s.jpg' % (farm, server, image_id, secret)
+                }
+            except:
+                message = {
+                    'fallback': 'Foods everywhere',
+                    'title': 'Foods everywhere!',
+                    'text': 'Here\'s your food!',
+                    'image_url': 'https://c2.staticflickr.com/2/1694/24145596374_0d225c0e74.jpg'
+                }
 
         return JsonResponse({
             'response_type': 'in_channel',
